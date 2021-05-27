a38ad1eeaa8cbd5f1d6dd14b0c857372d92287b8
Lucas Simonini <lucas.simonini.br@AC-LPT-00395.box>
2021-05-27 13:45:50 -0300

    ˜first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# location-query-service
---
+++ src/test/java/com/example/locationqueryservice/LocationQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locationqueryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationQueryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

cae264134528e28c5e9459b911388487a7c456dd
lcssimonini <lcs.simonini@gmail.com>
2021-06-02 16:40:22 -0300

    adding spring cloud contract

---
+++ src/test/java/com/simo/locationqueryservice/util/contract/ControllerBaseTest.java
@@ -0,0 +1,14 @@
+package com.simo.locationqueryservice.util.contract;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+public class ControllerBaseTest {
+
+  @Autowired private MockMvc mvc;
+}
